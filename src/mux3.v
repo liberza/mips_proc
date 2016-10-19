53e2556688a8d645b3c2fed7d808e33cc4669d4d
Nick Levesque <nick@levesque.io>
2016-10-18 21:05:20 -0400

    add mux3.v

---
+++ src/mux3.v
@@ -0,0 +1,18 @@
+module mux3(
+    input wire[1:0] ctrl,
+    input wire[31:0] s0,
+    input wire[31:0] s1,
+    input wire[31:0] s2
+    output wire[31:0] out);
+
+    always @(*) begin
+        if (ctrl == 2'b00) begin
+            out = s0;
+        end else if (ctrl == 2'b01) begin
+            out = s1;
+        end else begin
+            out = s2;
+        end
+    end
+endmodule
+
