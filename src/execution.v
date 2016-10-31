c248c3f15c431a311d9f4ec9c711c51bfe227b30
Christopher David Martin <christopher.d.martin@maine.edu>
2016-10-31 19:28:16 -0400

    changed d2_out to one bit in execution

--- src/execution.v
+++ src/execution.v
@@ -3,7 +3,7 @@ module execution(
 	input wire[31:0] d2_in,
 	input wire[3:0] aluctrl,
 	output wire[31:0] d1_out,
-	output wire[31:0] d2_out
+	output wire[1:0] d2_out
 	);
 
 	assign d2_out = d1_in + d2_in;
